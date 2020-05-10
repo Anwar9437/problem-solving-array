de7cfc8eea8eb02548249c6ae4c6d7a50ffa586b
Anwar Hossain <anwar9437@gmail.com>
2020-05-10 20:11:18 +0600

    change count

--- count_Frequency.c
+++ count_Frequency.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-    int array[100], n, count, freq[100];
+    int array[100], n, counter, freq[100];
 
     printf("Enter number of elements in array\n");
     scanf("%d", &n);
@@ -11,8 +11,8 @@ int main() {
     for (int i = 0; i <n ; i++) {
         for (int j = i+1; j < n; j++) {
             if(array[i]==array[j]){
-                count++;
-                freq[i]=count;
+                counter++;
+                freq[i]=counter;
                 printf("Frequency %d = %d\n", array[i],freq[i]);
             }
         }
