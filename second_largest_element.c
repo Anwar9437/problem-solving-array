0abd24a0c8782249b3dc0ae544867fd2a8726abd
Anwar Hossain <anwar9437@gmail.com>
2020-05-11 19:39:42 +0600

    comment delete

--- second_largest_element.c
+++ second_largest_element.c
@@ -3,11 +3,13 @@ int main(){
     int n, min, max1, max2;
     scanf("%d", &n);
     int a[n];
+
     for (int i = 0; i < n ; i++) {
         scanf("%d", &a[i]);
     }
     min = a[0];
     max1 = max2 = a[0];
+
     for (int i = 0; i < n; i++) {
         if(min>a[i])
             min = a[i];
