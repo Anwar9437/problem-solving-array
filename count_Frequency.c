3f879355cd975d31da1959b135152b847425a4a0
Anwar Hossain <anwar9437@gmail.com>
2020-05-11 21:05:27 +0600

    Count frequency

--- count_Frequency.c
+++ count_Frequency.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-    int array[100], n, counter, freq[100];
+    int array[100], n, count = 0, freq[100];
 
     printf("Enter number of elements in array\n");
     scanf("%d", &n);
@@ -8,15 +8,28 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
-    for (int i = 0; i <n ; i++) {
-        for (int j = i+1; j < n; j++) {
-            if(array[i]==array[j]){
-                counter++;
-                freq[i]=counter;
-                printf("Frequency %d = %d\n", array[i],freq[i]);
+    for (int i = 0; i < n; i++) {
+        count = 1;
+        if (array[i] != -1) {
+
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                    array[j] = -1;
+                }
+                {
+                freq[i] = count;
+                }
             }
+
         }
 
-    }
 
+    }
+    for (int k = 0; k < n ; ++k) {
+        if(array[k]!=-1){
+            printf("no of %d is %d\n", array[k],freq[k]);
+        }
+        
+    }
 }
