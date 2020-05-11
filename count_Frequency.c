c5bb059c57af21a7ccace7f59937f7cb8280df59
Anwar Hossain <anwar9437@gmail.com>
2020-05-11 21:24:30 +0600

    small change

--- count_Frequency.c
+++ count_Frequency.c
@@ -14,11 +14,12 @@ int main() {
 
             for (int j = i + 1; j < n; j++) {
                 if (array[i] == array[j]) {
+
                     count++;
+
                     array[j] = -1;
-                }
-                {
-                freq[i] = count;
+
+                    freq[i] = count;
                 }
             }
 
